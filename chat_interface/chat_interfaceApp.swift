2d92e536fe98fad379f732e6f31304927d982765
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-16 11:00:51 +0530

    Initial Commit

---
+++ chat_interface/chat_interfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_interfaceApp.swift
+//  chat_interface
+//
+//  Created by Shaan on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct chat_interfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
